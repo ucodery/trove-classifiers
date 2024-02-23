058d50274b19d57bf1a400332c7dac1779ecbb79
ucodery <>
2024-02-23 02:08:07 +0000

    New trove classifiers

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ use std::str::Split;
 use strum_macros::{AsRefStr, Display, EnumString, IntoStaticStr};
 
 /// The version of the python package pypa/trove-classifiers that is captured by Classifier
-pub const PYPA_VERSION: &str = "2024.1.31";
+pub const PYPA_VERSION: &str = "2024.2.22";
 
 /// # Examples
 ///
@@ -1002,6 +1002,8 @@ pub enum Classifier {
     ProgrammingLanguage__Go,
     #[strum(serialize = "Programming Language :: Haskell")]
     ProgrammingLanguage__Haskell,
+    #[strum(serialize = "Programming Language :: Hy")]
+    ProgrammingLanguage__Hy,
     #[strum(serialize = "Programming Language :: Java")]
     ProgrammingLanguage__Java,
     #[strum(serialize = "Programming Language :: JavaScript")]
